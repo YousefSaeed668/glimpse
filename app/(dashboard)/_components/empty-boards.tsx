556032cc73f1bf04a7d272b6bc85cadf8c680549
YousefSaeed668 <yousefsaeed668@gmail.com>
2024-03-03 19:09:31 +0200

    create empty page for diffrent situation and finish create board functionality

---
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { toast } from "sonner";
+export const EmptyBoards = () => {
+  const { mutate, pending } = useApiMutation(api.board.create);
+  const { organization } = useOrganization();
+  const onClick = () => {
+    if (!organization) return;
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+        //TODO: redirect to the board
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image src="/note.svg" alt="Empty" width={100} height={100} />
+      <h2 className="text-2xl font-semibold mt-6">No results found!</h2>
+      <p className="text-muted-foreground text-sm mt-2">
+        Try searching for something else
+      </p>
+      <div className="mt-6">
+        <Button disabled={pending} size="lg" onClick={onClick}>
+          Create board
+        </Button>
+      </div>
+    </div>
+  );
+};
--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  providers: [
-    {
-      domain: "https://fluent-ibex-64.clerk.accounts.dev",
-      applicationID: "convex",
-    },
-  ],
-};
